60f0d8250061031c99af03c2559bfcc6ba7ff51c
Taweesin6201012620295 <68320677+Taweesin6201012620295@users.noreply.github.com>
2020-10-24 17:03:21 +0700

    Add files via upload

--- synthesizer GUI.py
+++ synthesizer GUI.py
@@ -28,7 +28,10 @@ Data1.set(time.strftime("%d/%m/%Y"))
 Time1.set(time.strftime("%H:%M:%S"))
 #============================Label with title =====================
 Label(ABC1, text="Synthesizer",font=('arial',25,'bold'),padx=8,pady=8,bd=4,bg="powder blue",
-fg="white",justify=CENTER).grid(row=0,column=0,columnspan=11)
+fg="white").grid(row=0,column=0,columnspan=11)
+
+C = Canvas(ABC1, height=200, width=200, bg = 'white' )
+C.grid(row=1,column=2)
 
 #==================================================================
 #txtDate=Entry(ABC1, textvariable=Data1, font=('arial',18,'bold'),bd=34,bg="powder blue",
@@ -39,8 +42,8 @@ fg="white",justify=CENTER).grid(row=0,column=0,columnspan=11)
 
 #txtDate=Entry(ABC1, textvariable=Time1, font=('arial',18,'bold'),bd=34,bg="powder blue",
 #fg="white",width=28,justify=CENTER).grid(row=1,column=2,columnspan=1)
-btnCs=Button(ABC, height=2, width=6,bd=4, text="Rec", font=('arial',18,'bold'),bg="black",fg="white")
-btnCs.grid(row=0,column=2,padx=5,pady=5)
+btnCs=Button(ABC, height=2, width=4,bd=4, text="Rec", font=('arial',18,'bold'),bg="black",fg="white")
+btnCs.grid(row=0,column=1,padx=5,pady=5)
 
 btnCs=Button(ABC1, height=2, width=6,bd=4, text="sin", font=('arial',18,'bold'),bg="black",fg="white")
 btnCs.grid(row=1,column=4,padx=5,pady=5)
@@ -51,52 +54,52 @@ btnCs.grid(row=1,column=5,padx=5,pady=5)
 btnCs=Button(ABC1, height=2, width=6,bd=4, text="Triangle", font=('arial',18,'bold'),bg="black",fg="white")
 btnCs.grid(row=1,column=6,padx=5,pady=5)
 
-btnCs=Button(ABC1, height=2, width=4,bd=4, text="cos", font=('arial',18,'bold'),bg="black",fg="white")
+btnCs=Button(ABC1, height=2, width=7,bd=4, text="sawtooth", font=('arial',18,'bold'),bg="black",fg="white")
 btnCs.grid(row=1,column=7,padx=5,pady=5)
 
-btnCs=Button(ABC1, height=2, width=4,bd=4, text="cord1", font=('arial',18,'bold'),bg="black",fg="white")
+btnCs=Button(ABC1, height=2, width=4,bd=4, text="Style1", font=('arial',18,'bold'),bg="black",fg="white")
 btnCs.grid(row=2,column=4,padx=5,pady=5)
 
-btnCs=Button(ABC1, height=2, width=4,bd=4, text="cord2", font=('arial',18,'bold'),bg="black",fg="white")
+btnCs=Button(ABC1, height=2, width=4,bd=4, text="Style2", font=('arial',18,'bold'),bg="black",fg="white")
 btnCs.grid(row=2,column=5,padx=5,pady=5)
 
-btnCs=Button(ABC1, height=2, width=4,bd=4, text="cord3", font=('arial',18,'bold'),bg="black",fg="white")
+btnCs=Button(ABC1, height=2, width=4,bd=4, text="Style3", font=('arial',18,'bold'),bg="black",fg="white")
 btnCs.grid(row=2,column=6,padx=5,pady=5)
 
-btnCs=Button(ABC1, height=2, width=4,bd=4, text="cord4", font=('arial',18,'bold'),bg="black",fg="white")
+btnCs=Button(ABC1, height=2, width=4,bd=4, text="Style4", font=('arial',18,'bold'),bg="black",fg="white")
 btnCs.grid(row=2,column=7,padx=5,pady=5)
 
 #==============================Black Button===============================
-btnCs=Button(ABC3, height=6, width=6,bd=4, text="C#", font=('arial',18,'bold'),bg="black",fg="white")
+btnCs=Button(ABC3, height=4, width=6,bd=4, text="C#", font=('arial',18,'bold'),bg="black",fg="white")
 btnCs.grid(row=0,column=1,padx=5,pady=5)
 
-btnDs=Button(ABC3, height=6, width=6,bd=4 , text="D#", font=('arial',18,'bold'),bg="black",fg="white")
+btnDs=Button(ABC3, height=4, width=6,bd=4 , text="D#", font=('arial',18,'bold'),bg="black",fg="white")
 btnDs.grid(row=0,column=2,padx=5,pady=5)
 btnSpace=Button(ABC3, state=DISABLED, width=2,height=6 ,bg = "powder blue",relief=FLAT)
 btnSpace.grid(row=0,column=3,padx=0,pady=0)
-btnFs=Button(ABC3, height=6, width=6,bd=4 , text="F#", font=('arial',18,'bold'),bg="black",fg="white")
+btnFs=Button(ABC3, height=4, width=6,bd=4 , text="F#", font=('arial',18,'bold'),bg="black",fg="white")
 btnFs.grid(row=0,column=4,padx=5,pady=5)
 
-btnGs=Button(ABC3, height=6, width=6,bd=4 , text="G#", font=('arial',18,'bold'),bg="black",fg="white")
+btnGs=Button(ABC3, height=4, width=6,bd=4 , text="G#", font=('arial',18,'bold'),bg="black",fg="white")
 btnGs.grid(row=0,column=5,padx=5,pady=5)
-btnBb=Button(ABC3, height=6, width=6,bd=4 , text="Bb", font=('arial',18,'bold'),bg="black",fg="white")
+btnBb=Button(ABC3, height=4, width=6,bd=4 , text="Bb", font=('arial',18,'bold'),bg="black",fg="white")
 btnBb.grid(row=0,column=6,padx=5,pady=5)
 
 #===============================White button==========================
-btnC=Button(ABC3, height=6, width=8, text="C", font=('arial',18,'bold'),bg="white",fg="black")
+btnC=Button(ABC3, height=4, width=8, text="C", font=('arial',18,'bold'),bg="white",fg="black")
 btnC.grid(row=1,column=0,padx=5,pady=5)
-btnD=Button(ABC3, height=6, width=8, text="D", font=('arial',18,'bold'),bg="white",fg="black")
+btnD=Button(ABC3, height=4, width=8, text="D", font=('arial',18,'bold'),bg="white",fg="black")
 btnD.grid(row=1,column=1,padx=5,pady=5)
-btnE=Button(ABC3, height=6, width=8, text="E", font=('arial',18,'bold'),bg="white",fg="black")
+btnE=Button(ABC3, height=4, width=8, text="E", font=('arial',18,'bold'),bg="white",fg="black")
 btnE.grid(row=1,column=2,padx=5,pady=5)
-btnF=Button(ABC3, height=6, width=8, text="F", font=('arial',18,'bold'),bg="white",fg="black")
+btnF=Button(ABC3, height=4, width=8, text="F", font=('arial',18,'bold'),bg="white",fg="black")
 btnF.grid(row=1,column=3,padx=5,pady=5)
 
-btnG=Button(ABC3, height=6, width=8, text="G", font=('arial',18,'bold'),bg="white",fg="black")
+btnG=Button(ABC3, height=4, width=8, text="G", font=('arial',18,'bold'),bg="white",fg="black")
 btnG.grid(row=1,column=4,padx=5,pady=5)
-btnA=Button(ABC3, height=6, width=8, text="A", font=('arial',18,'bold'),bg="white",fg="black")
+btnA=Button(ABC3, height=4, width=8, text="A", font=('arial',18,'bold'),bg="white",fg="black")
 btnA.grid(row=1,column=5,padx=5,pady=5)
-btnB=Button(ABC3, height=6, width=8, text="B", font=('arial',18,'bold'),bg="white",fg="black")
+btnB=Button(ABC3, height=4, width=8, text="B", font=('arial',18,'bold'),bg="white",fg="black")
 btnB.grid(row=1,column=6,padx=5,pady=5)
 
 #==================================================================
